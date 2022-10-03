a2bf8ff9bedb2bb404fb8eaff9aae2babeac6869
gaconkzk <gaconkzk@gmail.com>
2022-10-03 10:47:30 +0700

    update

--- packages/flelte-g6/src/lib/index.ts
+++ packages/flelte-g6/src/lib/index.ts
@@ -3,4 +3,3 @@ export { default as G6Graph } from '$lib/components/G6Graph.svelte'
 export * from '$lib/utils/objects'
 
 export { registerNode, registerEdge } from '@antv/g6'
-export type { GraphData, NodeConfig, EdgeConfig, IPoint, ModelConfig, IGroup } from '@antv/g6'
