db6d7ab6b8144da3ef1f328239c4659c10ae2129
gaconkzk <gaconkzk@gmail.com>
2022-10-03 08:44:40 +0700

    export type IGroup

--- packages/flelte-g6/src/lib/index.ts
+++ packages/flelte-g6/src/lib/index.ts
@@ -3,4 +3,4 @@ export { default as G6Graph } from '$lib/components/G6Graph.svelte'
 export * from '$lib/utils/objects'
 
 export { registerNode, registerEdge } from '@antv/g6'
-export type { GraphData, NodeConfig, EdgeConfig, IPoint, ModelConfig } from '@antv/g6'
+export type { GraphData, NodeConfig, EdgeConfig, IPoint, ModelConfig, IGroup } from '@antv/g6'
